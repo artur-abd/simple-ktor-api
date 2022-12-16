dcafb3f0b632264d172a2d694bf68ac8ab922310
Artur Abdulkhakov <arturabdulkhakov1@gmail.com>
2022-12-16 14:53:09 +0300

    Add database initialization

--- src/main/kotlin/com/task/Application.kt
+++ src/main/kotlin/com/task/Application.kt
@@ -1,5 +1,19 @@
 package com.task
 
-fun main() {
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.Database
 
+
+fun initDatabase() {
+    val config = HikariConfig("db.properties")
+    val dataSource = HikariDataSource(config)
+    val flyway = Flyway.configure().dataSource(dataSource).load()
+    flyway.migrate()
+
+    Database.connect(dataSource)
+}
+fun main() {
+    initDatabase()
 }
