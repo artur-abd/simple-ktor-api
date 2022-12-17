2314ef94b1e98708e983ed77a933a861d3773c9c
Artur Abdulkhakov <arturabdulkhakov1@gmail.com>
2022-12-17 09:16:59 +0300

    Feat make phone_number and email unique

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Users
+create TABLE Users
 (
     id           INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name         VARCHAR(255) not NULL,
     surname      VARCHAR(255) not NULL,
     patronymic   VARCHAR(255),
-    phone_number VARCHAR(50),
-    email        VARCHAR(320)
+    phone_number VARCHAR(50) UNIQUE,
+    email        VARCHAR(320) UNIQUE
 
 );
