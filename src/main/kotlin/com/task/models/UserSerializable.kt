2302073ae0fdd54472a0bd4785aed8e2f01e06a4
Artur Abdulkhakov <arturabdulkhakov1@gmail.com>
2022-12-17 14:04:49 +0300

    Add UserSerializable class

---
+++ src/main/kotlin/com/task/models/UserSerializable.kt
@@ -0,0 +1,18 @@
+package com.task.models
+
+import com.task.storage.User
+import kotlinx.serialization.Serializable
+
+@Serializable
+/**
+ * [User] class serializable representation
+ */
+data class UserSerializable(
+    val name: String,
+    val surname: String,
+    val patronymic: String?,
+    val phoneNumber: String,
+    val email: String
+) {
+    constructor(user: User) : this(user.name, user.surname, user.patronymic, user.phoneNumber, user.email)
+}
