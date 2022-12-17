b58a65cbfa9aa393cc4acbd049b1de2f3d70ceb5
Artur Abdulkhakov <arturabdulkhakov1@gmail.com>
2022-12-17 14:08:50 +0300

    Add http api

--- src/main/kotlin/com/task/Application.kt
+++ src/main/kotlin/com/task/Application.kt
@@ -9,11 +9,22 @@ import org.jetbrains.exposed.sql.Database
 fun initDatabase() {
     val config = HikariConfig("db.properties")
     val dataSource = HikariDataSource(config)
-    val flyway = Flyway.configure().dataSource(dataSource).load()
-    flyway.migrate()
-
+    Flyway.configure().dataSource(dataSource).load().apply { migrate() }
     Database.connect(dataSource)
 }
+
 fun main() {
     initDatabase()
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            isLenient = true
+            prettyPrint = true
+        })
+    }
+    userRouting()
+
 }
---
+++ src/main/kotlin/com/task/routing/UserRouting.kt
@@ -0,0 +1,60 @@
+package com.task.routing
+
+import com.task.models.UserSerializable
+import com.task.storage.User
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.jetbrains.exposed.exceptions.ExposedSQLException
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun Application.userRouting() {
+    routing {
+        route("/user") {
+            post("/add") {
+                val newUser: UserSerializable =
+                    kotlin.runCatching { call.receiveNullable<UserSerializable>() }.getOrNull()
+                        ?: return@post call.respond(HttpStatusCode.MethodNotAllowed, "invalid input")
+                var id: Int? = null
+                try {
+                    transaction {
+                        id = User.new {
+                            name = newUser.name
+                            surname = newUser.surname
+                            patronymic = newUser.patronymic
+                            phoneNumber = newUser.phoneNumber
+                            email = newUser.email
+                        }.id.value
+
+                    }
+                } catch (ex: ExposedSQLException) {
+                    if (ex.message?.contains(Regex("phone_number|email")) == true) {
+                        return@post call.respond(
+                            HttpStatusCode.MethodNotAllowed, "email or phone number already used"
+                        )
+                    }
+                }
+                if (id != null) {
+                    call.respond("user added with id=$id")
+                } else {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+            get("/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull() ?: return@get call.respond(
+                    HttpStatusCode.BadRequest, "invalid id supplied"
+                )
+                val user = transaction {
+                    User.findById(id)
+                }
+                if (user == null) {
+                    call.respond(HttpStatusCode.NotFound, "user not found")
+                } else {
+                    call.respond(UserSerializable(user))
+                }
+            }
+        }
+    }
+}
